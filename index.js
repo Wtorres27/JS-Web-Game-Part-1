f1f1a4b652dc0f7889548b1c4106bef57c06129c
Wtorres27 <costellobenida@gmail.com>
2023-09-22 22:26:10 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imageUrl, left, right, bottom) {
+  let imageObject = document.createElement("img");
+  imageObject.src = imageUrl;
+  imageObject.style.position = "fixed";
+  imageObject.style.left = left + "px";
+  imageObject.style.right = right + "px";
+  imageObject.style.bottom = bottom + "px";
+  imageObject.style.width = "px";
+  document.body.append(imageObject);
+}
+function grass(imageUrl, height, width) {
+  let imageObject = document.createElement("img");
+  imageObject.src = imageUrl;
+  imageObject.c
+  imageObject.style.position = "fixed";
+  imageObject.style.height = height + "px";
+  imageObject.style.width = width + "px";
+
+  document.body.append(imageObject);
+}
+
+grass("assets/grass.png", "900", "1890");
+newImage("", "", "", "");
+newImage("assets/tree.png", "", "", "1");
+newImage("assets/tree.png", "300", "", "");
+
+newImage("assets/tree.png", "", "30", "");
+newImage("assets/tree.png", "", "450", "30");
+newImage("assets/well.png", "", "500", "500");
+newImage("assets/tree.png", "", "500", "600");
+newImage("assets/green-character.gif", "100", "20", "100");
+newImage("assets/green-character.gif", "800", "900");
+
