fd4c1dcaead0708c314c2dac353de5ce1dfe33d5
Wtorres27 <costellobenida@gmail.com>
2023-09-24 20:23:17 -0700

    part 4 incomplete

--- index.js
+++ index.js
@@ -135,3 +135,54 @@ newImage("assets/pine-tree.png", "1600", "500");
 newItem("assets/sword.png", "550", "480");
 newItem("assets/shield.png", "1700", "400");
 newItem("assets/staff.png", "850", "300");
+
+
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+
+let direction = null;
+let x = 100;
+let y = 250;
+
+if(direction === 'west'){
+    x = x - 1
+}
+if(direction === 'north'){
+    y = y + 1
+}
+if(direction === 'east'){
+    x = x + 1
+}
+if(direction === 'south'){
+    y = y - 1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+
+setInterval(function(){ 
+  if(direction === 'west'){
+      x = x - 1
+  }
+  if(direction === 'north'){
+      y = y + 1
+  }
+  if(direction === 'east'){
+      x = x + 1
+  }
+  if(direction === 'south'){
+      y = y - 1
+  }
+  character.style.left = x + 'px'
+  character.style.bottom = y + 'px'
+}, 1)
+
+                           
+
+document.addEventListener('keydown', function(e){
+
+})
+
+
+
